819e65008352999d31ebb5fcf76836e4706b907d
Nattapat  Juthaprachakul <nattapat.juthaprachakul17@imperial.ac.uk>
2018-02-20 14:10:26 +0000

    move file to work with env

---
+++ src/layers.py
@@ -0,0 +1,206 @@
+import numpy as np
+
+def linear_forward(X, W, b):
+    """
+    Computes the forward pass for a linear (fully-connected) layer.
+
+    Args:
+    - X: A numpy array of shape (N, d_1, ..., d_K) incoming data
+    - W: Anumpy array of shape (K, M) of weights
+    - b: A numpy array of shape (M, ) of biases
+
+    Returns:
+    - out: linear transformation to the incoming data
+
+
+    """
+    out = None
+    """
+    TODO: Implement the linear forward pass. Store your result in `out`.
+    Tip: Think how X needs to be reshaped.
+    """
+    ###########################################################################
+    #                           BEGIN OF YOUR CODE                            #
+    ###########################################################################
+    X_reshaped = X.reshape(X.shape[0],-1)
+    # print(X_reshaped,"dddddddddddddddddddddddd")
+    out = np.dot(X_reshaped,W) + b.T
+
+    ###########################################################################
+    #                            END OF YOUR CODE                             #
+    ###########################################################################
+    return out
+
+
+def linear_backward(dout, X, W, b):
+    """
+    Computes the forward pass for a linear (fully-connected) layer.
+
+    Args:
+    - dout: Upstream derivative, of shape (N, M)
+    - X: A numpy array of shape (N, d_1, ..., d_K) incoming data
+    - W: Anumpy array of shape (K, M) of weights
+    - b: A numpy array of shape (M, ) of biases
+
+    Returns (as tuple):
+    - dX: A numpy array of shape (N, d1, ..., d_k), gradient with respect to x
+    - dW: A numpy array of shape (D, M), gradient with respect to W
+    - db: A nump array of shape (M,), gradient with respect to b
+    """
+    dX, dW, db = None, None, None
+    """
+    TODO: Implement the linear backward pass. Store your results of the
+    gradients in `dX`, `dW`, `db`.
+    """
+    ###########################################################################
+    #                           BEGIN OF YOUR CODE                            #
+    ###########################################################################
+    number_of_example = X.shape[0]
+    X_original_shape = X.shape
+
+    dX = np.dot(dout, w.T)
+    dX = dX.reshape(X_original_shape)
+
+
+    ###########################################################################
+    #                            END OF YOUR CODE                             #
+    ###########################################################################
+
+    return dX, dW, db
+
+
+def relu_forward(X):
+    """
+    Computes the forward pass for rectified linear unit (ReLU) layer.
+    Args:
+    - X: Input, an numpy array of any shape
+    Returns:
+    - out: An numpy array, same shape as X
+    """
+    out = None
+    """
+    TODO: Implement the ReLU forward pass. Store your result in out.
+    """
+    ###########################################################################
+    #                           BEGIN OF YOUR CODE                            #
+    ###########################################################################
+    out = X.copy()  # Must use copy in numpy to avoid pass by reference.
+    out[out < 0] = 0
+
+    #out = 1/(1+np.exp(-X))  Sigmoid functions
+
+    # ReLU function
+    for index_1 in range(X.shape[0]):
+        for index_2 in range(X.shape[1]):
+            out[index_1][index_2] = max(0,out[index_1][index_2])
+    ###########################################################################
+    #                            END OF YOUR CODE                             #
+    ###########################################################################
+    return out
+
+
+def relu_backward(dout, X):
+    """
+    Computes the backward pass for rectified linear unit (ReLU) layer.
+    Args:
+    - dout: Upstream derivative, an numpy array of any shape
+    - X: Input, an numpy array with the same shape as dout
+    Returns:
+    - dX: A numpy array, derivative with respect to X
+    """
+    dX = None
+    """
+    TODO: Implement the ReLU backward pass. Store your result in out.
+    """
+    ###########################################################################
+    #                           BEGIN OF YOUR CODE                            #
+    ###########################################################################
+    dX = np.greater(X,0).astype(int)
+
+    ###########################################################################
+    #                            END OF YOUR CODE                             #
+    ###########################################################################
+    return dX
+
+
+def dropout_forward(X, p=0.5, train=True, seed=42):
+    """
+    Compute f
+    Args:
+    - X: Input data, a numpy array of any shape.
+    - p: Dropout parameter. We drop each neuron output with probability p.
+    Default is p=0.5.
+    - train: Mode of dropout. If train is True, then perform dropout;
+      Otherwise train is False (= test mode). Default is train=True.
+
+    Returns (as a tuple):
+    - out: Output of dropout applied to X, same shape as X.
+    - mask: In training mode, mask is the dropout
+      mask that was used to multiply the input; in test mode, mask is None.
+    """
+    out = None
+    mask = None
+    if seed:
+        np.random.seed(seed)
+    """
+    TODO: Implement the inverted dropout forward pass. Make sure to consider
+    both train and test case. Pay attention scaling the activation function.
+    """
+    ###########################################################################
+    #                           BEGIN OF YOUR CODE                            #
+    ###########################################################################
+    q = 1 - p
+    out = np.array(X.shape)
+
+    if train == True:
+        mask = np.random.rand(X.shape)
+        for row in mask:
+            for cell in row:
+                if cell < p:
+                    cell = 0
+        mask *= (1/q)
+        out = mask * x
+
+
+    else: #testing mode
+        out = x
+
+    ###########################################################################
+    #                            END OF YOUR CODE                             #
+    ###########################################################################
+    return out, mask
+
+
+def dropout_backward(dout, mask, p=0.5, train=True):
+    """
+    Compute the backward pass for dropout
+    Args:
+    - dout: Upstream derivative, a numpy array of any shape
+    - mask: In training mode, mask is the dropout mask that was used to
+      multiply the input; in test mode, mask is None.
+    - p: Dropout parameter. We drop each neuron output with probability p.
+    Default is p=0.5.
+    - train: Mode of dropout. If train is True, then perform dropout;
+      Otherwise train is False (= test mode). Default is train=True.
+
+    Returns:
+    - dX: A numpy array, derivative with respect to X
+    """
+    dX = None
+    """
+    TODO: Implement the inverted backward pass for dropout. Make sure to
+    consider both train and test case.
+    """
+    ###########################################################################
+    #                           BEGIN OF YOUR CODE                            #
+    ###########################################################################
+
+    if train == True:
+        dx = dout * mask
+    else: #testing mode
+        dx = dout
+
+    ###########################################################################
+    #                            END OF YOUR CODE                             #
+    ###########################################################################
+    return dX
