d73dbcd5561b94bdaefc8399be7e0ca4a8579c67
George  Wright <george.wright17@imperial.ac.uk>
2018-03-02 14:26:46 +0000

    Replace train_fcnet.py

--- src/train_fcnet.py
+++ src/train_fcnet.py
@@ -6,6 +6,7 @@ from src.utils.data_utils import get_CIFAR10_data
 from src.utils.plot import plotGraphs
 from src.data_utils_fer2013 import get_FER2013_data
 import src.utils.analysis as an
+import pickle as pkl
 
 
 """
@@ -33,30 +34,48 @@ accuracy on the validation set.
 
 # plotGraphs(net, solver)
 
-INPUT_NODE = [888,888]
+INPUT_NODE = [1000,1000]
 # INPUT_NODE = [150,150,150,150,15,15, 400]
 #
 #
-data = get_FER2013_data(14355,14354 ,3588)
+data = get_FER2013_data(22967,5742,3588)
+print(data['X_train'][0])
+print(np.shape(data['X_train'][0]))
+
+
 targets = data['y_test']
 # #data = get_FER2013_data(49,1 ,0)
 #
 INPUT_DIMS = np.prod(data["X_train"].shape[1:])
 HIDDEN_DIMS = np.asarray(INPUT_NODE)
 NUM_CLASS = 7
-net = FullyConnectedNet(HIDDEN_DIMS,INPUT_DIMS,num_classes=NUM_CLASS,dropout=0.6,seed =300)
+#net = FullyConnectedNet(HIDDEN_DIMS,INPUT_DIMS,num_classes=NUM_CLASS,dropout=0.6,seed =300)
 net = FullyConnectedNet(HIDDEN_DIMS,INPUT_DIMS,num_classes=NUM_CLASS)
-#
+
 solver = Solver(net, data,update_rule='sgd_momentum',optim_config={\
-                 'learning_rate': 0.001, 'momentum': 0.0001},\
-             num_epochs=40,\
-             batch_size = 10,\
+                                                                   'learning_rate': 0.001, 'momentum': 0.5},\
+             num_epochs=20,\
+             batch_size = 100,\
              lr_decay=0.001,\
              print_every =1000)
-#solver.train()
+solver.train()
 plotGraphs(net, solver)
 
-probs = solver.model.loss(data['X_test'], None)
+################## OUTPUT TO PKL FILE ###########################
+
+output = open('net.pkl', 'wb')
+pkl.dump(solver.model, output, -1)
+output.close()
+
+################## READ PKL FILE ##################################
+
+pk = open('net.pkl', 'rb')
+test = pkl.load(pk)
+print(test)
+
+################## TEST NET ########################################
+
+probs = test.loss(data['X_test'], None)
 print(probs)
 classifications = an.getClassifications(probs)
 cm = an.confusionMatrix(classifications, targets, 7)
@@ -70,6 +89,8 @@ print("recall class 1: ", recall)
 print("precision class 1: ", precision)
 print("f1 class 1: ", f1measure)
 print("classification rate: ", classification)
+
+
 
 ##############################################################################
 #                             END OF YOUR CODE                               #
