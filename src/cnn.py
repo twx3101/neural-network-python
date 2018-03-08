b8012f951c4678a579c58ff48d297e049fe7714c
George  Wright <george.wright17@imperial.ac.uk>
2018-03-08 14:45:21 +0000

    code cleaning

--- src/cnn.py
+++ src/cnn.py
@@ -31,71 +31,7 @@ opt = optimizers.SGD(lr=0.01)
 model.compile(loss='categorical_crossentropy',
             optimizer=opt,
             metrics=['accuracy'])
-#create model
 
-# model = Sequential()
-# model.add(Convolution2D(32, (5,5),
-#                         activation="relu",
-#                         input_shape=(48,48,1)))
-# model.add(Convolution2D(32,(3,3),activation="relu"))
-# model.add(Convolution2D(32,(3,3),activation="relu"))
-# model.add(Convolution2D(32,(3,3),activation="relu"))
-# model.add(Convolution2D(64,(3,3),activation="relu"))
-# model.add(MaxPooling2D(pool_size=(2,2)))
-# model.add(Flatten())
-# model.add(Dense(1024, activation="relu"))
-# model.add(Dropout(0.2))
-# model.add(Dense(1024, activation="relu"))
-# model.add(Dense(7, activation="softmax"))
-# model.compile(loss="categorical_crossentropy",
-#               optimizer="adam",
-#               metrics=['accuracy'])
-
-# img_rows, img_cols = 48, 48
-# model = Sequential()
-# model.add(Convolution2D(36, (5, 5), border_mode='valid',
-#                         input_shape=(img_rows, img_cols,1)))
-# model.add(keras.layers.advanced_activations.PReLU(init='zero', weights=None))
-# model.add(keras.layers.convolutional.ZeroPadding2D(padding=(2, 2), dim_ordering='tf'))
-# model.add(MaxPooling2D(pool_size=(5, 5),strides=(2, 2)))
-#
-# model.add(keras.layers.convolutional.ZeroPadding2D(padding=(1, 1), dim_ordering='tf'))
-# model.add(Convolution2D(36, (3, 3)))
-# model.add(keras.layers.advanced_activations.PReLU(init='zero', weights=None))
-# model.add(keras.layers.convolutional.ZeroPadding2D(padding=(1, 1), dim_ordering='tf'))
-# model.add(Convolution2D(36, (3, 3)))
-# model.add(keras.layers.advanced_activations.PReLU(init='zero', weights=None))
-# model.add(keras.layers.convolutional.AveragePooling2D(pool_size=(3, 3),strides=(2, 2)))
-#
-# model.add(keras.layers.convolutional.ZeroPadding2D(padding=(1, 1), dim_ordering='tf'))
-# model.add(Convolution2D(64, (3, 3)))
-# model.add(keras.layers.advanced_activations.PReLU(init='zero', weights=None))
-# model.add(keras.layers.convolutional.ZeroPadding2D(padding=(1, 1), dim_ordering='tf'))
-# model.add(Convolution2D(64, (3, 3)))
-# model.add(keras.layers.advanced_activations.PReLU(init='zero', weights=None))
-#
-# model.add(keras.layers.convolutional.ZeroPadding2D(padding=(1, 1), dim_ordering='tf'))
-# model.add(keras.layers.convolutional.AveragePooling2D(pool_size=(3, 3),strides=(2, 2)))
-#
-# model.add(Flatten())
-# model.add(Dense(1024))
-# model.add(keras.layers.advanced_activations.PReLU(init='zero', weights=None))
-# model.add(Dropout(0.2))
-# model.add(Dense(1024))
-# model.add(keras.layers.advanced_activations.PReLU(init='zero', weights=None))
-# model.add(Dropout(0.2))
-#
-#
-# model.add(Dense(7))
-#
-#
-# model.add(Activation('softmax'))
-#
-# ada = Adadelta(lr=0.1, rho=0.95, epsilon=1e-08)
-# model.compile(loss='categorical_crossentropy',
-#               optimizer=ada,
-#               metrics=['accuracy'])
-#load data
 
 data = get_FER2013_data(20000, 8000, 9, True)
 
